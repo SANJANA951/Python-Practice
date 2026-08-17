51daf6c9a91c672adf60394ae645988ec9eb51b7
Sanjana Saroj <sanjanasaroj4848@gmail.com>
2026-08-17 21:04:53 +0530

    Add files via upload

---
+++ CH9-FILE_APPEND.py
@@ -0,0 +1,7 @@
+n = "Hello i am prince"
+
+f = open("file.txt", "a")
+
+f.write(n)
+
+f.close()
---
+++ CH9-FILE_WRITE.py
@@ -0,0 +1,7 @@
+st = "Hey prince you can do it!"
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+
+f.close()
